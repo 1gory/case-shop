d0b1cbf54a5c06ba24ec785a76c7f39c86612ede
bahoss <bahobek@gmail.com>
2017-07-18 14:18:29 +0600

    Add Fail and Succes messages of Feedback

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './index.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MainPage from './components/MainPage';
 import Catalog from './components/Catalog';
+import FeedbackFail from './components/MainPage/FeedbackForm/FeedbackFail';
+import FeedbackSucces from './components/MainPage/FeedbackForm/FeedbackSucces';
 
 const ScrollToTop = () => {
   window.scrollTo(0, 0);
@@ -19,6 +21,8 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={MainPage}/>
             <Route path="/catalog" component={Catalog}/>
+            <Route path="/feedbackfail" component={FeedbackFail}/>
+            <Route path="/feedbacksucces" component={FeedbackSucces}/>
           </Switch>
         </div>
       </Router>
---
+++ src/components/MainPage/FeedbackForm/FeedbackSucces/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background-color: #3b3b3b;
+  text-align: center;
+  padding: 100px 40px 140px 40px;
+`;
+
+const Message = styled.h3`
+  color: #59cb78;
+  font-family: 'Lato-Light';
+  font-size: 16px;
+  font-weight: normal;
+`;
+
+export default () => (
+  <Wrapper>
+    <Message>Спасибо! Мы свяжемся с вами как можно скорее.</Message>
+  </Wrapper>
+);
