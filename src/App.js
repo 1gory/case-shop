d90d175fad9cca2da180c659388994778cd9424d
1gory <igor.pershin.me@gmail.com>
2017-07-18 18:43:41 +0400

    Add missed packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import './index.css';
 import { Route, Switch } from 'react-router-dom';
 import MainPage from './components/MainPage';
