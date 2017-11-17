6d29e3a4c85d177c160d1b2ed889e8231c002594
1gory <igor.pershin.me@gmail.com>
2017-11-17 16:22:53 +0300

    improve yandex kassa

--- src/components/Payment/index.jsx
+++ src/components/Payment/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import queryString from 'query-string';
+import GhostButton from '../generic/Form/Buttons/GhostButton';
 import preloader from './preloader.svg';
 import icon from './icon.svg';
 
@@ -9,7 +10,7 @@ const Wrapper = styled.div`
 
 const PreloaderContainer = styled.div`
   position: fixed;
-  top: 50%;
+  top: 35%;
   left: 50%;
   transform: translate(-50%, -50%);
 `;
@@ -30,23 +31,51 @@ const IconsWrapper = styled.div`
   position: relative;
 `;
 
+const ButtonsWrapper = styled.div`
+  width: 230px;
+  position: relative;
+  margin: 0 auto;
+`;
+
+const Button = styled(GhostButton)`
+  width: 100%;
+  margin-top: 15px;
+`;
+
+const Prepayment = styled(Button)`
+`;
+
+const FullPayment = styled(Button)`
+  background-color: #89da65;
+`;
+
 export default class extends Component {
   constructor() {
     super();
     this.state = {
       customerNumber: '',
     };
+
+    this.handleSendForm = this.handleSendForm.bind(this);
   }
 
   componentWillMount() {
     const parsed = queryString.parse(this.props.location.search);
     this.state = {
+      value: 500,
       customerNumber: parsed.customerNumber,
+      transferredValue: parsed.value,
     };
   }
 
-  componentDidMount() {
-    setTimeout(() => (this.FormRef.submit()), 1500);
+  handleSendForm(value) {
+    if (value) {
+      this.setState({
+        value: value * 0.9,
+      }, this.FormRef.submit());
+    } else {
+      this.FormRef.submit();
+    }
   }
 
   render() {
@@ -57,13 +86,29 @@ export default class extends Component {
             <Preloader src={preloader} alt="" />
             <Icon src={icon} alt="" />
           </IconsWrapper>
+
+          <form ref={(el) => { this.FormRef = el; }} action="https://money.yandex.ru/eshop.xml" method="POST">
+            <input name="shopId" value="159341" type="hidden" />
+            <input name="scid" value="151001" type="hidden" />
+            <input name="sum" value={this.state.value} type="hidden" />
+
+            <ButtonsWrapper>
+              <Prepayment
+                name="customerNumber"
+                onClick={() => { this.handleSendForm(); }}
+              >
+                Предоплата
+              </Prepayment>
+
+              <FullPayment
+                name="customerNumber"
+                onClick={() => { this.handleSendForm(this.state.transferredValue); }}
+              >
+                Полная оплата (-10%)
+              </FullPayment>
+            </ButtonsWrapper>
+          </form>
         </PreloaderContainer>
-        <form ref={(el) => { this.FormRef = el; }} action="https://money.yandex.ru/eshop.xml" method="POST">
-          <input name="shopId" value="88501" type="hidden" />
-          <input name="scid" value="95712" type="hidden" />
-          <input name="sum" value="500" type="hidden" />
-          <input name="customerNumber" value={this.state.customerNumber} type="hidden" />
-        </form>
       </Wrapper>
     );
   }
--- src/components/generic/Form/Buttons/GhostButton.jsx
+++ src/components/generic/Form/Buttons/GhostButton.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export default styled.button`
   background-color: #3b3b3b;
+  cursor: pointer;
   border: solid 1px #fff;
   border-radius: 20px;
   font-family: 'Lato-Light';
