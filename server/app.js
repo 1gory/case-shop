15c36681c2b1a731d7cdabcf07d79c79564d0a5a
1gory <igor.pershin.me@gmail.com>
2017-11-28 15:47:20 +0300

    fix ssr

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ import morgan from 'morgan';
 import path from 'path';
 import index from './routes/index';
 import api from './routes/api';
+import loader from './loader';
 import { logger } from './logger';
 
 logger.level = 'debug';
@@ -33,6 +34,8 @@ app.use(express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(require('./routes/payments/payments').default);
 
 app.use('/api', api);
+
+app.use('/', loader);
 
 app.use((err, req, res, next) => {
   logger.error(err.stack);
