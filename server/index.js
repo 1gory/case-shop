955af468fb7cd284fdc281ded0bc3ca1928a3c30
1gory <igor.pershin.me@gmail.com>
2018-01-20 03:36:44 +0400

    add script for adding defer attribute for build index.html

--- server/index.js
+++ server/index.js
@@ -1,16 +1,6 @@
-import https from 'https';
-import fs from 'fs';
 import app from './app';
 
-const key = fs.readFileSync('server/encryption/privkey.pem');
-const cert = fs.readFileSync('server/encryption/fullchain.pem');
-
 const PORT = process.env.PORT || 3001;
-const HTTPS_PORT = process.env.HTTPS_PORT || 443;
-
-if (PORT !== 3001) {
-  https.createServer({ key, cert }, app).listen(HTTPS_PORT);
-}
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
