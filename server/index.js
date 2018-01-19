859a0bbcecd26740768d3828202e4ce1e583f215
1gory <igor.pershin.me@gmail.com>
2018-01-20 01:39:39 +0400

    add custom port for https

--- server/index.js
+++ server/index.js
@@ -6,9 +6,9 @@ const key = fs.readFileSync('server/encryption/privkey.pem');
 const cert = fs.readFileSync('server/encryption/fullchain.pem');
 
 const PORT = process.env.PORT || 3001;
+const HTTPS_PORT = process.env.HTTPS_PORT || 443;
 
 if (PORT !== 3001) {
-  const HTTPS_PORT = 443;
   https.createServer({ key, cert }, app).listen(HTTPS_PORT);
 }
 
