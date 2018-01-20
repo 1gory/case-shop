76f7189de5ee20eb5bc4143ac80520a7a0db7796
1gory <igor.pershin.me@gmail.com>
2018-01-20 04:02:04 +0400

    fix bash script for addig defer atttibute to build/index.html

--- addDefer.sh
+++ addDefer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # add defer attribute for script to build/index.js
 
-sed -E -i '' 's/(src="\/static\/js\/main\.[a-z0-9]*\.js")/\1 defer/g' build/index.html
+sed -E -i 's/(src="\/static\/js\/main\.[a-z0-9]*\.js")/\1 defer/g' build/index.html
