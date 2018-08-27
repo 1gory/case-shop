2a292f472d08e30c5652d2b8d7b9eaf488602551
1gory <igor.pershin.me@gmail.com>
2018-08-27 17:09:02 +0300

    fix order mail message

--- server/services/mailer.js
+++ server/services/mailer.js
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer';
-import { config } from '../config';
-import { uploadFileFormLogger, logger } from '../logger';
+import {config} from '../config';
+import {uploadFileFormLogger, logger} from '../logger';
 
 uploadFileFormLogger.level = 'debug';
 logger.level = 'debug';
@@ -22,20 +22,25 @@ export default (mailType, data) => {
     },
   });
 
-  const textMessage = `Здравствуйте, ${data.customerName}! Пишу вам по поводу заказа на деревянный чехол. Вы выбрали ${data.material} чехол на ${data.model} и прислали фото. Подготовить для вас макет?`;
+  const material = data.material === 'light' ? 'светлое' : 'темное';
+  const textMessage = `Здравствуйте, ${data.customerName}! Пишу вам по поводу заказа на деревянный чехол. Вы выбрали ${material} чехол на ${data.model} и прислали фото. Подготовить для вас макет?`;
 
   const macMessage = encodeURI(`whatsapp://send?text=${textMessage}&amp;phone=${data.phone}`);
   const windowsMessage = `${encodeURI(`https://api.whatsapp.com/send?text=${textMessage}`)}&phone=${data.phone.replace(/[-+\s()]/g, '')}&source=&data=`;
 
   let output = '';
   output += `Телефон: ${data.phone} <br>`;
-  output += `<a href=${macMessage}>Whatsapp Mac</a><br>`;
-  output += `<a href=${windowsMessage}>Whatsapp Windows</a><br>`;
+  output += data.messenger ? `messenger: ${data.messenger} <br>` : '';
+  if (data.messenger === 'whatsapp') {
+    output += `<a href=${macMessage}>Whatsapp Mac</a><br>`;
+    output += `<a href=${windowsMessage}>Whatsapp Windows</a><br>`;
+  } else {
+    output += `${textMessage}<br>`;
+  }
   output += data.model ? `Модель: ${data.model} <br>` : '';
-  output += data.material ? `Дерево: ${data.material} <br>` : '';
+  output += data.material ? `Дерево: ${material} <br>` : '';
   output += data.customerName ? `ФИО: ${data.customerName} <br>` : '';
   output += data.image ? `image: ${data.image} <br>` : '';
-  output += data.messenger ? `messenger: ${data.messenger} <br>` : '';
   output += `Страница заказа: ${data.referer} <br>`;
   output += data.message ? `message: ${data.message} <br>` : '';
   output += data.prepayment ? `Внесена предоплата: ${data.prepayment} <br>` : '';
