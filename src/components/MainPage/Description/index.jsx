f97e55e6ced63c964e7ef0275664a2727f4bf272
1gory <igor.pershin.me@gmail.com>
2018-09-03 15:32:24 +0300

    add links to the products pages

--- src/components/MainPage/Description/index.jsx
+++ src/components/MainPage/Description/index.jsx
@@ -26,7 +26,31 @@ const Link = styled(RouterLink)`
 export default () => (
   <Wrapper>
     <DescriptionWrapper>
-      У нас вы можете купить деревянные чехлы с гравировкой на айфон любых моделей.<br />
+      У нас вы можете купить деревянные чехлы с гравировкой на айфон любых моделей:<br />
+      <Link to="/product/derevyannyj-chekhol-dlya-iphone-5">
+        iPhone 5
+      </Link>, <Link to="/product/derevyannyj-chekhol-dlya-iphone-5s">
+        iPhone 5s
+      </Link>, <Link to="/product/derevyannyj-chekhol-dlya-iphone-se">
+        iPhone SE
+      </Link>, <Link to="/product/derevyannyj-chekhol-dlya-iphone-6">
+        iPhone 6
+      </Link>, <Link to="/product/derevyannyj-chekhol-dlya-iphone-6s">
+        iPhone 6s
+      </Link>, <Link to="/product/derevyannyj-chekhol-dlya-iphone-6s-plus">
+        iPhone 6s Plus
+      </Link>, <Link to="/product/derevyannyj-chekhol-dlya-iphone-7">
+        iPhone 7
+      </Link>, <Link to="/product/derevyannyj-chekhol-dlya-iphone-7-plus">
+        iPhone 7 Plus
+      </Link>, <Link to="/product/derevyannyj-chekhol-dlya-iphone-8">
+        iPhone 8
+      </Link>, <Link to="/product/derevyannyj-chekhol-dlya-iphone-8-plus">
+        iPhone 8 Plus
+      </Link>, <Link to="/product/derevyannyj-chekhol-dlya-iphone-x">
+        iPhone X
+      </Link>
+      <br />
       Мы находимся в городе Москве, <Link to="/delivery">
       но отправляем заказы по всей России и СНГ</Link>.<br />
       Быстрая доставка по Москве. Самовывоз - бесплатно.
